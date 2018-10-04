2d38cc5e43293f3adf8527b1e55ed60c572bb168
jon <stricoff92@gmail.com>
2018-10-03 23:45:39 -0400

    updated readme, updated tweet logic

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # twit-scrambler
+
+### A Twitter bot that randomly scrambles a user's tweet.
+
+## How it works
+
+1) A user's most recent tweet is selected and parsed with natural language tool kit.
+2) A random selection of recent tweets are selected and parsed with natural language tool kit.
+3) A random selection of verbs, verbs ending in "ing", nouns, plural nouns, and proper nouns from the target tweet are swapped out with corresponding word types from the pool of words from the recent tweets.
+4) The mashed up tweet is posted.
+
+## Setup
+Create creds.json
+```bash
+$ cd twit-scrambler
+$ echo '{
+    "consumer_key": "YOUR_TWITTER_CONSUMER_KEY",
+    "consumer_secret": "YOUR_TWITTER_CONSUMER_SECRET_KEY",
+    "access_token_key": "YOUR_TWITTER_ACCESS_TOKEN_KEY",
+    "access_token_secret": "YOUR_TWITTER_ACCESS_TOKEN_SECRET"
+}' > creds.json
+```
+
+Add dictionaries to TWITTER_ACCOUNTS list
+| dict key      | required      | desc.  |
+| ------------- |:-------------:| -----:|
+| handle        | yes           | (string) twitter user name |
+| mix_perc      | yes           | (float) % chance that a qualified word gets swapped out |
+| lookback      | no            | (int) max age in days for tweets |
+| tweets_to_mix | no            | (int) number of recent tweets to use for building out word pool to draw from |
+
+```python
+
+# Set the twitter accounts to pull tweets from.
+TWITTER_ACCOUNTS = [
+    {'handle':'myTwitterHandle', 'lookback':14, 'tweets_to_mix':5, 'mix_perc':0.65}
+]
+```
+
+## Run script
+```bash
+$ python twit_scrambler.py
+```
+
--- twit_scrambler.py
+++ twit_scrambler.py
@@ -1,5 +1,6 @@
 
 import datetime as dt
+import http.client, urllib
 import json
 import os
 import random
@@ -18,18 +19,19 @@ DATA_FILE_NAMING_CONV = PATH + '/%s_data.txt'
 DEFAULT_LOOKBACK = 14
 DEFAULT_TWEETS_TO_MIX = 10
 MIN_WORDS = 10
-MIN_SWAP_WORD_LEN = 3
+MIN_SWAP_WORD_LEN = 4
 TWEETS_TO_PULL = 50
+TWEET_CHAR_LIMIT = 279
 TWT_FIELDS = 'full_text'
-POST_TO_TWITTER = False
+POST_TO_TWITTER = True
 WORD = 0
 WTYPE = 1
-TARGET_TWEET_OVERRIDE = None
+TARGET_TWEET_OVERRIDE = None #'The united states will build a wall betweet itself and Mexico. Mark my words!'
 
 
 # Set the twitter accounts to pull tweets from.
 TWITTER_ACCOUNTS = [
-    {'handle':'realDonaldTrump', 'lookback':14, 'tweets_to_mix':5, 'mix_perc':0.65}
+    {'handle':'realDonaldTrump', 'lookback':14, 'tweets_to_mix':5, 'mix_perc':0.5}
 ]
 
 TYPES_TOSWAP = (
@@ -37,32 +39,70 @@ TYPES_TOSWAP = (
     'VBG',  # Verbs ending in 'ING'
     'NN',   # Nouns
     'NNS',  # Plural Nouns
-    'NNP', # Proper Nouns
+    'NNP',  # Proper Nouns
+    'JJ',   # Adjective
 )
 
 SWAP_BLACK_LIST = (
     '@', 'of', 'in', 'at', 't', 'doesn', 'can', '-', ':', '?', '[', ']', '{', '}',
-    'be', 'do', ',', '.', '"', '\'', '`'
+    'be', 'do', ',', '.', '"', '\'', '`', 'great'
 )
+
 
 def skip_word(word):
     return (
-        word in SWAP_BLACK_LIST
+        word.lower() in SWAP_BLACK_LIST
         or bool(re.match(r'^\d+$', word))
         or bool(re.match(r'^\d\d\:\d\d$', word))
     )
 
 
+def clean_word_array(word_array):
+    # NLTK isn't perfect. example: 'United States' gets broken up into 2 separate words but we'd like to
+    # treat it like 1x Proper Noun.
+    # Implement logic in this function to fix these errors.
+
+    # 2nd to last element is the final string that should appear in the string
+    # last element is the word type
+    # elements 0 through length-3 are the strings to search for
+    forced_pairs = [
+        ('united', 'states', 'United States', 'NNP'),
+        ('brett', 'kavanaugh', 'Brett Kavanaugh', 'NNP'),
+        ('mike', 'pense', 'Mike Pense', 'NNP'),
+        ('jeff', 'sessions', 'Jeff Sessions', 'NNP'),
+        ('donald', 'trump', 'Donald Trump', 'NNP'),
+        ('west', 'virginia', 'West Virginia', 'NNP'),
+        ('north', 'carolina', 'North Carolina', 'NNP'),
+        ('south', 'carolina', 'South Carolina', 'NNP'),
+        ('new', 'york', 'New York', 'NNP'),
+        ('new', 'jersey', 'New Jersey', 'NNP'),
+        ('new', 'mexico', 'New Mexico', 'NNP'),
+    ]
+    
+    words = [w[WORD] for w in word_array]
+    cleaned_words = [w[WORD].lower().replace(' ', '') for w in word_array]
+    
+    for fixes in forced_pairs:
+        if all(w in cleaned_words for w in fixes[:-2]):
+            print("FIXING", fixes)
+            ix = cleaned_words.index(fixes[0])
+            word_array = [tup for tup in word_array if tup[WORD] not in fixes[:-2]]
+            word_array.insert(ix, tuple(fixes[-2:]))
+    
+    return word_array
+
+
+
 def build_mashed_tweet(target_tweet, mix, perc):
     # given a tweet and a mixed bag of words, mash up the tweet
-    target_tweet_parts = nltk.pos_tag(nltk.word_tokenize(target_tweet))
+    target_tweet_parts = clean_word_array(nltk.pos_tag(nltk.word_tokenize(target_tweet)))
     print(target_tweet_parts)
     
     # create mashup_map of {word_type: available_worlds[]}
     # {'NNP':['Avenatti', 'Putin'], ...}
     mix_tweet_parts = []
     for twt in mix:
-        mix_tweet_parts += nltk.pos_tag(nltk.word_tokenize(twt))
+        mix_tweet_parts += clean_word_array(nltk.pos_tag(nltk.word_tokenize(twt)))
     mashup_map = {}
     for word in mix_tweet_parts:
         if word[WORD].lower() in SWAP_BLACK_LIST or len(word[WORD]) <= MIN_SWAP_WORD_LEN:
@@ -77,9 +117,10 @@ def build_mashed_tweet(target_tweet, mix, perc):
     
     # Create new Tweet
     mashed_tweet = []
+    swaps_performed = 0
     for ix, word in enumerate(target_tweet_parts):
         if (ix and word[WTYPE] in TYPES_TOSWAP 
-                and len(word[WTYPE]) >= MIN_SWAP_WORD_LEN
+                and len(word[WORD]) >= MIN_SWAP_WORD_LEN
                 and word[WTYPE] in mashup_map 
                 and not skip_word(word[WORD])
                 and random.random() <= perc):
@@ -87,7 +128,8 @@ def build_mashed_tweet(target_tweet, mix, perc):
             if len(mashup_map[word[WTYPE]]):
                 rand_word = mashup_map[word[WTYPE]].pop(random.randint(0, len(mashup_map[word[WTYPE]])-1))
                 mashed_tweet.append(rand_word)
-                print('swaping "%s" with "%s"' % (word, rand_word))
+                swaps_performed += 1
+                print('SWAPPING "%s" with "%s"' % (word, rand_word))
             else:
                 print("skipping swap of", word)
                 mashed_tweet.append(word[WORD])
@@ -95,7 +137,14 @@ def build_mashed_tweet(target_tweet, mix, perc):
             print("skipping swap of", word)
             mashed_tweet.append(word[WORD])
     
-    return ' '.join(mashed_tweet)
+    mashed_tweet_str = '"' + ' '.join(mashed_tweet) +'"\n\n-'+twit['handle']+' (sort of)'
+    mashed_tweet_str = mashed_tweet_str.replace(' , ', ', ')\
+                                        .replace(' . ', '. ')\
+                                        .replace(' ’ ', '’')\
+                                        .replace(' \' ', '\'')\
+                                        .replace(' !', '!')
+    print('swaps_performed', swaps_performed)
+    return mashed_tweet_str
 
     
 
@@ -124,6 +173,10 @@ def main(twit, api):
     except IOError:
         used_tweets = []
     
+    if results[0]['id_str'] in used_tweets:
+        print("already used this tweet! BYE!")
+        return
+    
 
     # Remove Tweets we've already used
     new_results = [r for r in results if r['id_str'] not in used_tweets]
@@ -143,11 +196,27 @@ def main(twit, api):
     print('\ntweets to mix in:', '\n'.join(r[TWT_FIELDS] for r in mix_tweet))
 
     # Generate new tweet 
-    new_tweet = build_mashed_tweet(target_tweet[TWT_FIELDS], [t[TWT_FIELDS] for t in mix_tweet], twit['mix_perc'])
+    new_tweet = build_mashed_tweet(TARGET_TWEET_OVERRIDE or target_tweet[TWT_FIELDS], 
+                                    [t[TWT_FIELDS] for t in mix_tweet], twit['mix_perc'])
     print('new mashed tweet\n', new_tweet)
+
+
+    # Post Tweet to Twitter
+    def truncate(string):
+        if len(string) <= TWEET_CHAR_LIMIT:
+            return string
+        else:
+            return string[0:TWEET_CHAR_LIMIT-2] + '..'
+    if POST_TO_TWITTER:
+        api.PostUpdate(status=truncate(new_tweet))
+
+        # Update txt file with target tweet ID
+        used_tweets.insert(0, target_tweet['id_str'])
+        used_tweets[0:500]
+        with open(DATA_FILE_NAMING_CONV % twit['handle'], 'w') as f:
+            for tid in used_tweets:
+                f.write(tid+'\n')
     
-
-
 
 if __name__ == '__main__':
     
